de43a6c2e30b9702825cb46140c57531f5767063
JPZ <jeanpaulzune@gmail.com>
2018-02-06 20:43:59 -0500

    Changded reducer syntax, removed concatantion

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,13 +1,9 @@
-import { FETCH_WEATHER } from '../actions/index'
+import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action) {
   switch (action.type) {
-    case FETCH_WEATHER:
-      return state.concat([action.payload.data]);
-
-      break;
-    default:
-
+  case FETCH_WEATHER:
+      return [ action.paload.data, ...state ];
   }
   return state;
 }
