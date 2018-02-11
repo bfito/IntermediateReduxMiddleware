b96db1002070a54acd3e1b4c24327aca950c12d9
JPZ <jeanpaulzune@gmail.com>
2018-02-10 20:00:28 -0500

    Some incompatiblity going on with react and sparklines

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, Sparklinesline } from 'react-sparklines';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Sparklines data={temps} height={120} width={180}>
+            <Sparklinesline color="red" />
+          </Sparklines>
+        </td>
       </tr>
     );
   }
@@ -33,4 +41,4 @@ function mapStateToProps({ weather }) {
   return { weather }; // { weather: weather }
 }
 
-export default connect (mapStateToProps)(WeatherList)
+export default connect (mapStateToProps)(WeatherList);
